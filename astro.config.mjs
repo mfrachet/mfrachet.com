f579d62c80cd4b60713bc20de28a8a2cadce9427
mfrachet <undefined>
2023-11-02 13:14:06 +0100

    bump deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({ edgeMiddleware: true }),
 });

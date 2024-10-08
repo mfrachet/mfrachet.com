5b235c0925608c6531b473e2ddae73cd757bedd1
Marvin Frachet <marvin.frachet@protonmail.com>
2024-10-08 12:27:49 +0200

    anchors

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,37 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
+import rehypeSlug from "rehype-slug";
+
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [
+    tailwind(),
+    react(),
+    mdx({
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: "prepend",
+            content: {
+              type: "text",
+              value: "#",
+            },
+            headingProperties: {
+              className: ["anchor"],
+            },
+            properties: {
+              className: ["anchor-link"],
+            },
+          },
+        ],
+      ],
+    }),
+  ],
   output: "server",
   adapter: vercel(),
   prefetch: true,
